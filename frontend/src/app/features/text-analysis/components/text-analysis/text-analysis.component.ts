e5a97d88ab6fb834f7e4c2a801f5519688ab2b67
kevinwielander <e11908531@student.tuwien.ac.at>
2024-10-26 13:23:46 +0200

    add: initial frontend structure

---
+++ frontend/src/app/features/text-analysis/components/text-analysis/text-analysis.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-text-analysis',
+  standalone: true,
+  imports: [],
+  templateUrl: './text-analysis.component.html',
+  styleUrl: './text-analysis.component.sass'
+})
+export class TextAnalysisComponent {
+
+}
