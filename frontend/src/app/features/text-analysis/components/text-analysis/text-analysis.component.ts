bb6d976bfdd8710faee005fd5f43d202b0c8f81d
kevinwielander <e11908531@student.tuwien.ac.at>
2024-10-26 18:51:41 +0200

    add: notification service

--- frontend/src/app/features/text-analysis/components/text-analysis/text-analysis.component.ts
+++ frontend/src/app/features/text-analysis/components/text-analysis/text-analysis.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {TextAnalyzerService} from "../../services/text-analyzer.service";
 import {FormsModule} from "@angular/forms";
 import {CommonModule, NgFor} from "@angular/common";
+import {ToastrService} from "ngx-toastr";
 
 @Component({
   selector: 'app-text-analysis',
@@ -20,17 +21,23 @@ export class TextAnalysisComponent {
   isOnline: boolean = false;
   analysisResults: { character: string, count: number }[] = [];
 
-  constructor(private textAnalyzerService: TextAnalyzerService) {}
+  constructor
+  (
+    private textAnalyzerService: TextAnalyzerService,
+    private toastr: ToastrService
+  ){}
 
   analyzeText() {
     if (this.isOnline) {
       this.textAnalyzerService.analyzeOnline(this.inputText, this.analysisType).subscribe({
         next: result => this.updateAnalysisResults(result),
-        error: error => console.error('Error analyzing text online:', error)
+        error: error => this.toastr.error('Error analyzing text online')
       });
+      this.toastr.success('Text analyzed online!');
     } else {
       const result = this.textAnalyzerService.analyzeOffline(this.inputText, this.analysisType);
       this.updateAnalysisResults(result);
+      this.toastr.success('Text analyzed offline!');
     }
   }
 
